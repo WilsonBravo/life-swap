01af8aabb358826a0ca3409b14ab449481c32103
Wilson Bravo <bravoc.wilson@gmail.com>
2025-05-05 11:48:02 -0500

    feat: eas preview build config

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,12 @@ import type { ExpoConfig } from "@expo/config";
 export default ({ config }: { config: ExpoConfig }) => ({
   ...config,
   extra: {
+    ...config.extra,
     API_URL: process.env.API_URL,
     ENV: process.env.NODE_ENV,
+  },
+  android: {
+    ...config.android,
+    package: "com.mylemonlife.lifeswap",
   },
 });
